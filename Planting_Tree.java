718dc41ef81d2ae6c85f7b17bce666c904eb67a5
Riya Srivastava <riya196969@gmail.com>
2020-10-22 10:22:23 +0530

    Create Planting_Tree.java

---
+++ Planting_Tree.java
@@ -0,0 +1,32 @@
+// Define your tree class in this file!
+public class Tree{
+
+ public static void main(String[] args){
+    //We are going to plant a tree;
+    /* We’re going to introduce ourselves and print a tree to the screen. */
+    System.out.println("Hey there, I’m Ariel and I’m learning to code in Java!");
+    System.out.println("“I’m going to plant a tree today!” or “Ready to get my hands dirty!”");
+    System.out.println("   **  ");
+    System.out.println(" ****** ");
+    System.out.println("  *** ");
+    System.out.println("   *  ");
+    System.out.println("   *  ");
+    System.out.println();
+
+    System.out.println(" * ** *");
+    System.out.println(" ******");
+    System.out.println("  **** ");
+    System.out.println("   **  ");
+    System.out.println("   **  ");
+    System.out.println("~~~~~~~~~~");
+    System.out.println();
+
+    System.out.println(" ***** ");
+    System.out.println(" ***** ");
+    System.out.println(" ***** ");
+    System.out.println("   *  ");
+    System.out.println("   *  ");
+    System.out.println("########");
+  }
+
+}
